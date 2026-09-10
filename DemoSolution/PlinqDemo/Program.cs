0748ceeddf6737a3eacba18b459d8eafb1a1becd
JP <jp@jp.nl>
2026-09-10 15:43:14 +0200

    PLINQ

---
+++ DemoSolution/PlinqDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+List<int> lijstje = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+var watch = new Stopwatch();
+watch.Start();
+var filtered = lijstje.AsParallel().Where(x => HeleZwareFilterOperatie(x)).ToArray();
+watch.Stop();
+foreach (var item in filtered)
+{
+	Console.WriteLine($"item: {item}");
+}
+
+Console.WriteLine($"Elapsed milliseconds: {watch.ElapsedMilliseconds}");
+
+static bool HeleZwareFilterOperatie(int item)
+{
+	Console.WriteLine($"berekenen voor item {item} / {Thread.CurrentThread.ManagedThreadId}");
+	Thread.SpinWait(500_000_000);
+	return true;
+}
